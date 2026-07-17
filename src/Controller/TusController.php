5b1de3c5919b236d6fd100f18e14db6d1d879477
rloewenross <rloewenross@proton.me>
2026-07-17 12:35:26 -0500

    fix mistakes in TusController

--- src/Controller/TusController.php
+++ src/Controller/TusController.php
@@ -10,13 +10,13 @@ use Symfony\Component\Messenger\MessageBusInterface;
 use TusPhp\Tus\Server as TusServer;
 use TusPhp\Events\TusEvent;
 use App\PveClientFactory;
-use App\Message\DiskImportMessage;
 use App\Message\DiskUploadMessage;
 
 class TusController extends AbstractController {
     public function __construct(
         private PveClientFactory $pveClientFactory,
         private MessageBusInterface $bus,
+        private SluggerInterface $slugger,
     ) {}
 
     #[Route('/tus/', name: 'tus_post')]
@@ -24,9 +24,10 @@ class TusController extends AbstractController {
     public function tus(Request $request, TusServer $server): Response {
         $client = $this->pveClientFactory->fromSession($request->getSession());
         $bus = $this->bus;
-        $server->event()->addListener('tus-server.upload.complete', function (TusEvent $event) use ($bus, $client) {
-            $oldFilePath = $message->event->getFile()->getFilePath();
-            $safeFileName = $this->slugger->slug(basename($oldFilePath));
+        $slugger = $this->slugger;
+        $server->event()->addListener('tus-server.upload.complete', function (TusEvent $event) use ($bus, $slugger, $client) {
+            $oldFilePath = $event->getFile()->getFilePath();
+            $safeFileName = $slugger->slug(basename($oldFilePath));
             $fileName = $safeFileName . '-' . uniqid();
             $filePath = dirname($oldFilePath) . '/' . $fileName;
             rename($oldFilePath, $filePath);
