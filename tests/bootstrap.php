bbba49ff7b999c31ab17965986c79184b8a7e2f8
rloewenross <rloewenross@proton.me>
2026-08-08 13:10:09 -0500

    Add tests for ImportStatusController

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,3 +11,5 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+(new \Symfony\Component\Filesystem\Filesystem())->remove(__DIR__.'/../var/cache/test');
